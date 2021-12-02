2a29dea4b99c4ba14d82f45d85eb8b548507fdcc
Linh Tran <linhtanl51@gmail.com>
2021-12-01 22:23:41 -0800

    created Token contract

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,20 +2,33 @@
 pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
+/** msg: global var inside Ethereum 
+ */
 contract Token is ERC20 {
-  //add minter variable
+  // initialize a public minter variable with address data type
+  address public minter;
 
-  //add minter changed event
+  // add minter changed event
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
+  constructor() public payable ERC20("Decentralized Bank Currency", "DCB") { // payable so we can send funds
+    // assign minter to sender
+    minter = msg.sender;
   }
 
-  //Add pass minter role function
+  // add ability to transfer minter role eg: from deployer => bank
+  function transferMinterRole(address dBank) public {
+    require(msg.sender == minter, "Error: mesg.sender is not a minter, only a minter can transfer mintership");
+    minter = dBank;
 
-  function mint(address account, uint256 amount) public {
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+  }
+
+  // create a mint wrapper to ensure that only minter above can mint token
+  function mint(address account, uint256 amount) public { // create a new token = minting
     //check if msg.sender have minter role
-		_mint(account, amount);
+    require(msg.sender == minter, "Error: msg.sender does not have minter role");
+		_mint(account, amount); // inherits _mint from ERC20
 	}
 }
