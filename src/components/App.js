69b6804476e17b897bbda9990db02ef3b21aa4fb
Linh Tran <linhtanl51@gmail.com>
2021-12-03 17:26:28 -0800

    fetch token and bank info from the  blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,88 +1,99 @@
-import { Tabs, Tab } from 'react-bootstrap'
-import dBank from '../abis/dBank.json'
-import React, { Component } from 'react';
-import Token from '../abis/Token.json'
-import dbank from '../dbank.png';
+import { Tabs, Tab } from 'react-bootstrap';
+import dBank from '../abis/dBank.json';
+import React, { Component, useEffect, useState } from 'react';
+import Token from '../abis/Token.json';
+import dbankLogo from '../dbank.png';
 import Web3 from 'web3';
 import './App.css';
 
-//h0m3w0rk - add new tab to check accrued interest
+const App = ({ dispatch }) => {
+  const [web3, setWeb3] = useState(null);
+  const [account, setAccount] = useState('');
+  const [token, setToken] = useState(null);
+  const [dBankInfo, setDBankInfo] = useState(null);
+  const [balance, setBalance] = useState(0);
+  const [dbankAddress, setDbankAddress] = useState(null);
 
-class App extends Component {
+  const loadBlockChainData = async (dispatch) => {
+    console.log('loading', dispatch);
+    if (typeof window.ethereum) {
+      const web3 = new Web3(window.ethereum);
+      const netId = await web3.eth.net.getId();
+      if (web3) setWeb3(web3);
 
-  async componentWillMount() {
-    await this.loadBlockchainData(this.props.dispatch)
-  }
+      const accounts = await web3.eth.getAccounts();
 
-  async loadBlockchainData(dispatch) {
+      if (accounts[0]) {
+        setAccount(accounts[0]);
+        const balance = await web3.eth.getBalance(accounts[0]);
+        setBalance(balance);
+      } else {
+        window.alert('Please connect to Metamask');
+      }
 
-    //check if MetaMask exists
-
-      //assign to values to variables: web3, netId, accounts
-
-      //check if account is detected, then load balance&setStates, elsepush alert
-
-      //in try block load contracts
-
-    //if MetaMask not exists push alert
-  }
-
-  async deposit(amount) {
-    //check if this.state.dbank is ok
-      //in try block call dBank deposit();
-  }
-
-  async withdraw(e) {
-    //prevent button from default click
-    //check if this.state.dbank is ok
-    //in try block call dBank withdraw();
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      web3: 'undefined',
-      account: '',
-      token: null,
-      dbank: null,
-      balance: 0,
-      dBankAddress: null
+      try {
+        // Create a new Token
+        const token = new web3.eth.Contract(
+          Token.abi,
+          Token.networks[netId].address
+        );
+        token && setToken(token);
+        // Create new bank
+        const bank = new web3.eth.Contract(
+          dBank.abi,
+          dBank.networks[netId].address
+        );
+        const bankAddress = dBank.networks[netId].address;
+        bank && setDBankInfo(bank) && setDbankAddress(bankAddress);
+      } catch (e) {
+        console.log('Error', e);
+        window.alert('Contracts not deployed to the current network');
+      }
+    } else {
+      window.alert('Please install Metamask');
     }
-  }
+  };
+  
+  useEffect(() => {
+    (async () => {
+      await loadBlockChainData(dispatch);
+    })();
+  }, [dispatch]);
 
-  render() {
-    return (
-      <div className='text-monospace'>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-        <img src={dbank} className="App-logo" alt="logo" height="32"/>
+  const deposit = async (amount) => console.log('deposited', amount);
+
+  const withdraw = async (e) => console.log('withdraw', e);
+
+  return (
+    <div className='text-monospace'>
+      <nav className='navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
+        <a
+          className='navbar-brand col-sm-3 col-md-2 mr-0'
+          href='http://www.dappuniversity.com/bootcamp'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img src={dbankLogo} className='App-logo' alt='logo' height='32' />
           <b>dBank</b>
         </a>
-        </nav>
-        <div className="container-fluid mt-5 text-center">
+      </nav>
+      <div className='container-fluid mt-5 text-center'>
         <br></br>
-          <h1>{/*add welcome msg*/}</h1>
-          <h2>{/*add user address*/}</h2>
-          <br></br>
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-              <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+        <h1>{/*add welcome msg*/}</h1>
+        <h2>{/*add user address*/}</h2>
+        <br></br>
+        <div className='row'>
+          <main role='main' className='col-lg-12 d-flex text-center'>
+            <div className='content mr-auto ml-auto'>
+              <Tabs defaultActiveKey='profile' id='uncontrolled-tab-example'>
                 {/*add Tab deposit*/}
                 {/*add Tab withdraw*/}
               </Tabs>
-              </div>
-            </main>
-          </div>
+            </div>
+          </main>
         </div>
       </div>
-    );
-  }
-}
-
+    </div>
+  );
+};
 export default App;
